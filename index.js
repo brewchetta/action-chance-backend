583423b2b9caa3a78f10fc3632e94da48c7c9974
Chett Tiller <chett.tiller@gmail.com>
2019-08-13 14:40:44 -0400

    various fixes and refactors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require(`mongodb`).MongoClient
 
 /* Variables */
 let userCount = 0
-let bg = {image: "https://clipart.wpblink.com/sites/default/files/wallpaper/drawn-forest/372214/drawn-forest-adobe-illustrator-372214-239163.jpg", mask: {color: '#7D7D7D', intensity: 25}}
+// let bg = {image: "https://clipart.wpblink.com/sites/default/files/wallpaper/drawn-forest/372214/drawn-forest-adobe-illustrator-372214-239163.jpg", mask: {color: '#7D7D7D', intensity: 25}}
 let displayMessage = '|||'
 
 /* Mongo */
@@ -17,14 +17,13 @@ const mongoConnect = callback => MongoClient.connect(`mongodb://localhost:27017/
 })
 
 // Get a record from a collection
-const getRecord = (name, dfault, callback) => mongoConnect(db => {
+const getRecord = (name, _default, callback) => mongoConnect(db => {
   db.collection("room1").findOne({name}, (err, result) => {
     if (err) throw err
     if (!result) {
-      console.log(result)
-      console.log(`---creating empty list of ${name} for room1---`)
-      db.collection("room1").insert({name, [name]: dfault })
-      callback(dfault)
+      console.log(`---creating new record of ${name} for room1---`)
+      db.collection("room1").insertOne({name, [name]: _default })
+      callback(_default)
     } else {
       callback(result[name])
     }
@@ -32,7 +31,7 @@ const getRecord = (name, dfault, callback) => mongoConnect(db => {
 })
 
 // Save a record to a colletion
-// --> uses find and replace rather than insert since it should already exist
+// --> uses findAndReplace rather than insert since it should already exist
 const saveRecord = (name, item, callback) => mongoConnect(db => {
   db.collection("room1").findOneAndReplace({name}, {name, [name]:item})
   callback(item)
@@ -42,20 +41,26 @@ const saveRecord = (name, item, callback) => mongoConnect(db => {
 
 // sends client all the up to date info once they're connected
 const onConnect = (socket) => {
+  // Increase user count
   userCount ++
+  // Emit message to all sockets and backend
   const message = `---a new user connected: ${socket.handshake.headers.origin}---`
   io.emit('user connect', {message, userCount})
   console.log(`\n---connection: ${socket.handshake.headers.origin} --------- ${userCount} users---`)
 
-  socket.emit('change background', bg)
+  // Send new socket all information it needs to get up with the others
   getRecord('participants', [], participants => socket.emit('change participants', participants))
   getRecord('activeParticipant', null, activeParticipant => socket.emit('change active participant', activeParticipant))
+  getRecord('bg', {image: "https://clipart.wpblink.com/sites/default/files/wallpaper/drawn-forest/372214/drawn-forest-adobe-illustrator-372214-239163.jpg", mask: {color: '#7D7D7D', intensity: 25}}, bg => socket.emit('change background', bg))
+  socket.emit('change display message', displayMessage)
 }
 
 // simple message logs when a client disconnects
 const onDisconnect = (clientIP) => {
-  const message = `---a user disconnected: ${clientIP}---`
+  // Decrease user count
   userCount --
+  // Send message to all other sockets
+  const message = `---a user disconnected: ${clientIP}---`
   console.log(`\n---disconnect: ${clientIP} --------- ${userCount} users---`)
   io.emit('user connect', {message, userCount})
 }
@@ -73,9 +78,10 @@ const onActiveParticipantChange = activeParticipant => {
   })
 }
 
-const onBGChange = newBG => {
-  bg = newBG
-  io.emit('change background', bg)
+const onBGChange = bg => {
+  saveRecord('bg', bg, () => {
+    io.emit('change background', bg)
+  })
 }
 
 const onDisplayMessageChange = newMessage => {
