0c881f6646bafbc12e94de83985544ddcd4c311b
Chett Tiller <chett.tiller@gmail.com>
2019-09-09 09:07:39 -0400

    Minor change

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ const mongo = require('./mongo')
 const io = require(`socket.io`)(con.http)
 
 /* CORS */
-io.set('origins', '*:*')
+io.set('origins', '*')
 
 /* Variables */
 const displayMessages = {}
